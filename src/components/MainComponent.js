394881ca862c5c3391932b6ff7f627d90877f815
danu263 <montes.alex@pucp.edu.pe>
2021-05-29 17:50:41 -0500

    react course done

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import About from './AboutComponent';
@@ -13,7 +12,15 @@ import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {addComment, fetchDishes, fetchComments, fetchPromos, postComment} from "../redux/ActionCreators";
+import {
+    addComment,
+    fetchDishes,
+    fetchComments,
+    fetchPromos,
+    postComment,
+    fetchLeaders,
+    postFeedback
+} from "../redux/ActionCreators";
 import {actions} from "react-redux-form";
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -25,7 +32,10 @@ const mapDispatchToProps = dispatch => ({
     fetchComments: () => dispatch(fetchComments()),
     fetchPromos: () => dispatch(fetchPromos()),
     postComment: (dishId, rating, author, comment) =>
-        dispatch(postComment(dishId, rating, author, comment))
+        dispatch(postComment(dishId, rating, author, comment)),
+    fetchLeaders: () => dispatch(fetchLeaders()),
+    postFeedback: (firstname, lastname, telnum, email, agree, contactType, message) =>
+        dispatch(postFeedback(firstname, lastname, telnum, email, agree, contactType, message))
 });
 
 const mapStateToProps = state => {
@@ -52,6 +62,7 @@ class Main extends Component {
       this.props.fetchDishes();
       this.props.fetchComments();
       this.props.fetchPromos();
+      this.props.fetchLeaders();
   }
 
     render() {
@@ -80,7 +91,9 @@ class Main extends Component {
               promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
               promoLoading={this.props.promotions.isLoading}
               promoErrMess={this.props.promotions.errMess}
-              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+              leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+              leaderLoading={this.props.leaders.isLoading}
+              leaderErrMess={this.props.leaders.errMess}
          />);
     }
 
@@ -91,7 +104,11 @@ class Main extends Component {
               <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
                   <Switch location={this.props.location}>
                       <Route path='/home' component={HomePage} />
-                      <Route exact path ='/contactus' component={()=> <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
+                      <Route exact path ='/contactus' component={()=>
+                          <Contact
+                              resetFeedbackForm={this.props.resetFeedbackForm}
+                              postFeedback={this.props.postFeedback}
+                          />} />
                       <Route path='/menu/:dishId' component={DishWithId} />
                       <Route exact path='/aboutus' component={()=> <About leaders={this.props.leaders}/>} />
                       <Route exact path='/menu' component={()=> <Menu dishes={this.props.dishes}/>} />
